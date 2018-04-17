16d7aca98bcb65480c97be25e9ba2bd048a13634
Luis Muñoz <luis.munoz@gotocme.com>
2018-04-16 21:01:55 -0300

    Added option to start printing XPath at an specific element.

--- README.md
+++ README.md
@@ -15,6 +15,14 @@ Taking [this XSD example from w3schools](https://www.w3schools.com/xml/schema_ex
 	/shiporder/item/note
 	/shiporder/item/quantity
 	/shiporder/item/price
+	
+Relative paths starting at an element are also possible
+
+    //shipto
+    //shipto/name
+    //shipto/address
+    //shipto/city
+    //shipto/country
 
 If -t option is passed it will print XML elements tree also
 
@@ -43,7 +51,8 @@ SYNOPSIS
 
 DESCRIPTION
 ===========
-Print XPath present on XML file. If an XSD file is provided and **xmlbeans** package is installed, try to create an XML instance and print the XPath from it.
+  Print XPath present on XML file. If an XSD file is provided and **xmlbeans** package is installed, try to create an XML instance and print the XPath from it.
+  If xsd and xml files are provided, xsd will be ignored
 
 OPTIONS
 ========
@@ -53,6 +62,15 @@ OPTIONS
         -d   xsd file path.
         -f   name of the root element to build xml from xsd.
     XML options
+        -s   absolute or relative path to start printing XPath from, e.g.: /shiporder/shipto //shipto 
         -t   print XML element tree as provided by xmllint 'du' shell command.
         -x   xml file, will take precedence over -d option.
 
+EXAMPLES
+========
+        # print all xpaths and elements tree
+        xml2xpath.sh -t -x test.xml
+        # print xpaths starting at //shipto element
+        xml2xpath.sh -s '//shipto' -x test.xml
+        # print xpaths from generated xml
+        xml2xpath.sh -d test.xsd -f shiporder
--- xml2xpath.sh
+++ xml2xpath.sh
@@ -7,10 +7,10 @@ script_name=$(basename $0)
 function print_help(){
 	cat<<EOF-MAN
 NAME
-$script_name - Print XPath present on xml or (if possible) xsd files.
+  $script_name - Print XPath present on xml or (if possible) xsd files.
 
 SYNOPSIS
-$script_name [-h] [-d file -f <tag name>] [-x file -t]
+  $script_name [-h] [-d file -f <tag name>] [-x file -t]
 
 DESCRIPTION
  Based on xmllint utility, try to build all possible XPaths from an XML instance. The latter could be constructed from a provided XSD file. 
@@ -22,18 +22,30 @@ OPTIONS
      -d   xsd file path.
      -f   name of the root element to build xml from xsd.
   XML options
+     -s   absolute or relative path to start printing XPath from, e.g.: /shiporder/shipto ,//shipto 
      -t   print XML element tree as provided by xmllint 'du' shell command.
      -x   xml file, will take precedence over -d option.
+     
+EXAMPLES
+	# print all xpaths and elements tree
+	xml2xpath.sh -t -x test.xml
+	# print xpaths starting at //shipto element
+	xml2xpath.sh -s '//shipto' -x test.xml
+	# print xpaths from generated xml
+	xml2xpath.sh -d test.xsd -f shiporder
+	
 EOF-MAN
 }
 
 function print_usage(){
-	echo "Usage: $script_name [-h] [-d file -f <tag name>] [-x file -t]"
+	echo "Usage: $script_name [-h] [-d file -f <tag name>] [-s path -x file -t]"
 }
 
 xsd=""
 xml_file=""
 tag1=""
+du_path="/"
+xprefix=""
 print_tree=0
 
 #---------------------------------------------------------------------------------------
@@ -67,7 +79,7 @@ function create_xml_instance(){
 #---------------------------------------------------------------------------------------
 function get_xml_tree(){
 	if [ -n "$xml_file" ]; then
-		echo "du /" | xmllint --shell "$xml_file" | grep -v '\/'
+		echo "du $du_path" | xmllint --shell "$xml_file" | grep -v '\/'
 	else
 		echo "ERROR: No XML file. Either provide an XSD to create an instance from (-d option) or pass the path to an XML valid file"
 		exit 1
@@ -79,26 +91,30 @@ function get_xml_tree(){
 #---------------------------------------------------------------------------------------
 function init_env(){
 	if [ -z "$xsd" ] && [ -z "$xml_file" ]; then
-		echo -e "FATAL: At least on of -d or -x must be provided.\n" > /dev/stderr
+		echo -e "FATAL: At least one of -d or -x must be provided.\n" > /dev/stderr
 		print_usage
 		exit 1
 	elif [ -n "$xsd" ] && [ -n "$xml_file" ]; then
 		echo -e "WARNING: both -d and -x were provided, -d will be ignored.\n" > /dev/stderr
 		xsd=''
 	fi
+	if echo "$du_path" | grep  -q '[/][/]' ; then
+		xprefix='/'
+	fi
 }
-while getopts d:f:htx: arg
+while getopts d:f:hs:tx: arg
 do
   case $arg in
     h) print_help; exit;;
 	d) xsd=$OPTARG;;
 	f) tag1=$OPTARG;;
+	s) du_path=$OPTARG;;
 	t) print_tree=1;;
 	x) xml_file=$OPTARG;;
     *) 
-        echo "Invalid option $arg"
-        print_help
-        exit 1;;
+       echo "Invalid option $arg"
+       print_help
+       exit 1;;
   esac
 done
 
@@ -116,22 +132,31 @@ fi
 indent_levels=$(get_indent_level "$xml_tree")
 max_level=$(echo "$indent_levels" | tail -n1)
 declare -a xpath_arr
+declare -a xpath_all
 
 # generate xpaths
-echo "$xml_tree" | while IFS='' read line; do
-    indent=$(echo "$line" | sed -nre 's/^( +).*/\1/p' | tr ' ' '.')
-    ilvl=$((${#indent}/2))
-    prev_lvl=$(($ilvl - 1))
-
-    if [ "$ilvl" -eq 0 ]; then
-        xpath_arr[0]="/$line"
-        xpath="${xpath_arr[0]}"
-    elif [ "$ilvl" -le "$max_level" ]; then
-        xpath="${xpath_arr[$prev_lvl]}/$(echo "$line" | tr -d ' ')"
-        xpath_arr[$ilvl]="$xpath"
-    fi
-    echo "$xpath"
-done
+echo "$xml_tree" | { 
+	while IFS='' read line; do
+	    indent=$(echo "$line" | sed -nre 's/^( +).*/\1/p' | tr ' ' '.')
+	    ilvl=$((${#indent}/2))
+	    prev_lvl=$(($ilvl - 1))
+	
+	    if [ "$ilvl" -eq 0 ]; then
+	        xpath_arr[0]="/$line"
+	        xpath="${xpath_arr[0]}"
+	    elif [ "$ilvl" -le "$max_level" ]; then
+	        xpath="${xpath_arr[$prev_lvl]}/$(echo "$line" | tr -d ' ')"
+	        xpath_arr[$ilvl]="$xpath"
+	    fi
+		#echo "$xprefix$xpath"
+		idx=${#xpath_all[*]}
+		xpath_all[$idx]="$xprefix$xpath"
+	done
+	
+	for i in "${xpath_all[@]}"; do 
+		echo "$i";
+	done
+}
 
 if [ -n "$xsd" ]; then
 	rm "$xml_file"
